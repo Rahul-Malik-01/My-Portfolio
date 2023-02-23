b97ceca38a37c4d00db4360042547814a0f52eef
Rahul-Malik-01 <malikrahul872@gmail.com>
2023-02-23 10:17:52 +0530

    about page added

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a').forEach(link => {
-  if(link.href.includes(`${activePage}`)){
-    link.classList.add('active');
-    console.log(link);
-  }
+document.querySelectorAll('.nav-link').
+forEach(link => {
+  if(link.href === window.location.href){
+    link.setAttribute('aria-current', 'page')
+  }
 })
