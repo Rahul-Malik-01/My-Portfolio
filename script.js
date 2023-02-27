bf54975be15c5792600d016b839f84052e9d92b7
Rahul-Malik-01 <malikrahul872@gmail.com>
2023-02-27 17:51:29 +0530

    animation added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.nav-link').
+document.querySelectorAll('.nav-link')
 forEach(link => {
   if(link.href === window.location.href){
     link.setAttribute('aria-current', 'page')
